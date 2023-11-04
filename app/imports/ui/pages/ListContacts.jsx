0a49d4e8944e171793e226d5733e97f87f7b088d
gahernan14 <143157163+gahernan14@users.noreply.github.com>
2023-11-03 21:25:50 -1000

    fixed list contact page

--- app/imports/ui/pages/ListContacts.jsx
+++ app/imports/ui/pages/ListContacts.jsx
@@ -12,7 +12,7 @@ const ListContacts = () => {
     // Note that this subscription will get cleaned up
     // when your component is unmounted or deps change.
     // Get access to Stuff documents.
-    const subscription = Meteor.subscribe(Contacts.PublicationName);
+    const subscription = Meteor.subscribe(Contacts.userPublicationName);
     // Determine if the subscription is ready
     const rdy = subscription.ready();
     // Get the Stuff documents
